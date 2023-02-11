7b4f74e894f4c84051a5552a3431923da6d11a84
IJ58LG <patryk.kwasek@ing.com>
2023-02-11 23:00:34 +0100

    Initialized project + added filtering and started adding stylings

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "emersoft-black": "#373a47",
+        "emersoft-green": "#15f2b8",
+      },
+    },
+  },
+  plugins: [],
+};
