91bd169395e1afd2242e88f7bef12a2768dfa85e
IJ58LG <patryk.kwasek@ing.com>
2023-02-21 20:36:06 +0100

    Fixed getPostsBySearchQuery

--- src/pages/api/getPostsBySearchQuery.ts
+++ src/pages/api/getPostsBySearchQuery.ts
@@ -1,21 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
-import { BlogPost } from '@/types/types';
+import { BlogPost } from "../../types/types";
 
-const filterPostsBySearchQuery = async (id: number, searchQuery: string) => {
+const filterPostsBySearchQuery = async (query: string) => {
   try {
-    const endpointUrl = process.env.NODE_ENV === 'development'
-      ? `${process.env.NEXT_PUBLIC_LOCALHOST_BASE_ENDPOINT}/api/getPosts`
-      : `${process.env.NEXT_PUBLIC_PRODUCTION_BASE_ENDPOINT}/api/getPosts`;
-
+    const endpointUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getPosts`;
     const response = await axios.get(endpointUrl);
     const blogData = response.data;
-
-    return blogData.posts
-      .filter((post: BlogPost) =>
-        post.categories.includes(id) &&
-        post.title.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+    return blogData.posts.filter((post: BlogPost) => post.title.toLowerCase().includes(query.toLowerCase()));
   } catch (error) {
     console.error(error);
     return [];
@@ -26,10 +18,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<BlogPost[]>
 ) {
-  const id = Number(req.query.id);
-  const query = req.query.query;
-  const searchQuery = Array.isArray(query) ? query[0] : query || '';
-  const posts = await filterPostsBySearchQuery(id, searchQuery);
-
+  const query = String(req.query.query);
+  const posts = await filterPostsBySearchQuery(query);
   res.status(200).json(posts);
 }
